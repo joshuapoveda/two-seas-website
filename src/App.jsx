7af5b25acbd2101d82f406da8a43fcfa336b14ab
Joshua Poveda <joshuapoveda9@gmail.com>
2023-06-29 09:18:52 -0400

    designing mobile-first

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
           </div>
           <div className="nav">
             <nav className="links-box">
-              <span className="text-sm md:text-2xl m-7 w-20  text-white md:hover:text-black">About</span>
-              <span className="text-sm pl-5 md:text-2xl m-7 w-20  text-white md:hover:text-black">Shop</span>
-              <span className="text-sm md:text-2xl m-7 w-20 text-white md:hover:text-black">Contact</span>
+              <span className="text-sm md:text-2xl md:m-7 w-20  text-white md:hover:text-black">About</span>
+              <span className="text-sm pl-5 md:p-0 md:text-2xl md:m-7 w-20  text-white md:hover:text-black">Shop</span>
+              <span className="text-sm md:text-2xl md:m-7 w-20 text-white md:hover:text-black">Contact</span>
               <div className="hidden md:flex">
                 < FaInstagramSquare className=" ml-6 text-black md:hover:text-brandCyan" size="60" />
               </div>
@@ -28,16 +28,25 @@ function App() {
 
       <div className="flex flex-grow border-4 border-yellow-500">
         <div className="flex flex-col flex-grow border-4 border-green-300">
-          <div className="flex h-1/3 border-4 border-black">BANNER</div>
+          <div className="flex h-72 md:h-2/3 border-8 border-black">
+            <h2 className="text-xl p-5 tracking-wider">Two Seas Cyclery</h2>
+            <div className="flex md:hidden">
+             <img
+                  className="border-4 border-red-600 h-auto sm:h-96 w-auto md:w-96"
+                  src="images/Two Seas Mascot Logo 1.png"
+                  alt="Image Description"
+                />
+                </div>
+          </div>
 
-          <div className="flex flex-grow border-4 border-cyan-800">
+          <div className="hidden md:flex flex-grow border-4 border-cyan-800">
             <div className="flex border-4 border-purple-600 flex-grow">
-              <div className="flex border-4 border-teal-300 w-2/5">
+              <div className="sm:flex border-4 hidden border-teal-300 sm:w-96 md:w-1/2">
                 <p>HELLO</p>
               </div>
               <div className="flex border-4 border-orange-400 flex-grow justify-end">
                 <img
-                  className="border-4 border-red-600 h-auto w-64 md:w-96"
+                  className="border-4 border-red-600 h-auto sm:h-96 w-auto md:w-96"
                   src="images/Two Seas Mascot Logo 1.png"
                   alt="Image Description"
                 />
