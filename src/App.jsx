d91bc32b3c6dd50307402c4115c8bc1844c8aa1a
Joshua Poveda <joshuapoveda9@gmail.com>
2023-07-12 08:47:24 -0400

    added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
